03b4ef1f4c1435a929651ffb835e39f4692a7c18
Jitendra Sabat <jitendrasabat.jitu@gmail.com>
2021-07-30 13:41:26 +0530

    style refinements

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -4,32 +4,30 @@ import './SearchResult.css';
 const SearchResult = (props) => {
     return <div className='search-result-container'>
         {props?.result?.map((city,index) => {
-    return <div key={index}>
-            <label id='city-name'>
-                City Name:
-            </label>
-            <div>{city.name}</div>
-            <label id='city-temp'>
-                City Temperature:
-            </label>
-            <div>{city.main.temp}</div>
-            <label id='city-wind'>
-                Wind:
-            </label>
-            <div>{city.main.temp}</div>
-            <label id='city-pressure'>
-                City Pressure:
-            </label>
-            <div>{city.main.temp}</div>
-            <label id='city-'>
-                City Cloud:
-            </label>
-            <div>{city.main.temp}</div>
-        </div>             
+                            return <div key={index} className='city-result-card'>
+                                    <label id='city-name'>
+                                        City Name:
+                                    </label>
+                                    <div>{city.name}</div>
+                                    <label id='city-temp'>
+                                        City Temperature:
+                                    </label>
+                                    <div>{city.main.temp}</div>
+                                    <label id='city-wind'>
+                                        Wind:
+                                    </label>
+                                    <div>{city.wind.speed} m/s</div>
+                                    <label id='city-pressure'>
+                                        City Pressure:
+                                    </label>
+                                    <div>{city.main.pressure} hpa</div>
+                                    <label id='city-'>
+                                        City Cloud:
+                                    </label>
+                                    <div>{city.clouds.all} %</div>
+                                </div>;            
 })}        
     </div>
 }
 
 export default SearchResult;
-
-// 
